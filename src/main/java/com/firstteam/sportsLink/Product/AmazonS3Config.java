9975a097e2b72feaf454f064ca044f9599284d8b
LoadingV4 <nk76802@naver.com>
2024-08-05 17:56:39 +0900

    update 내용일치, aws관련 주석 해제

--- src/main/java/com/firstteam/sportsLink/Product/AmazonS3Config.java
+++ src/main/java/com/firstteam/sportsLink/Product/AmazonS3Config.java
@@ -1,32 +1,32 @@
-// package com.firstteam.sportsLink.Product;
+package com.firstteam.sportsLink.Product;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import com.amazonaws.auth.AWSStaticCredentialsProvider;
-// import com.amazonaws.auth.BasicAWSCredentials;
-// import com.amazonaws.regions.Regions;
-// import com.amazonaws.services.s3.AmazonS3;
-// import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-// @Configuration
-// public class AmazonS3Config {
-//     @Value("${aws.access_key_id}")
-//     private String accessKeyId;
+@Configuration
+public class AmazonS3Config {
+    @Value("${aws.access_key_id}")
+    private String accessKeyId;
 
-//     @Value("${aws.secret_access_key}")
-//     private String secretAccessKey;
+    @Value("${aws.secret_access_key}")
+    private String secretAccessKey;
 
-//     @Value("${aws.region}")
-//     private String region;
+    @Value("${aws.region}")
+    private String region;
 
-//     @Bean
-//     public AmazonS3 amazonS3() {
-//         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
-//         return AmazonS3ClientBuilder.standard()
-//                 .withRegion(Regions.fromName(region))
-//                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-//                 .build();
-//     }
-// }
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
