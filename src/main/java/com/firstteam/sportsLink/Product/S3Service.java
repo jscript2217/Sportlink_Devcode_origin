cb0981753647dd8913cd16bfaab67ef99970d3e1
LoadingV4 <nk76802@naver.com>
2024-08-05 17:52:10 +0900

    update 코드일치

--- src/main/java/com/firstteam/sportsLink/Product/S3Service.java
+++ src/main/java/com/firstteam/sportsLink/Product/S3Service.java
@@ -1,33 +1,33 @@
-package com.firstteam.sportsLink.Product;
+// package com.firstteam.sportsLink.Product;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+// import com.amazonaws.services.s3.AmazonS3;
+// import com.amazonaws.services.s3.model.ObjectMetadata;
+// import com.amazonaws.services.s3.model.PutObjectRequest;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.UUID;
+// import java.io.IOException;
+// import java.util.UUID;
 
-@Service
-public class S3Service {
+// @Service
+// public class S3Service {
 
-        @Autowired
-        private AmazonS3 amazonS3;
+//         @Autowired
+//         private AmazonS3 amazonS3;
 
 
-        private String bucketName = "sportlink-image";
+//         private String bucketName = "sportlink-image";
 
-        public String uploadFile(MultipartFile file) throws IOException {
-            String fileName = generateFileName(file);
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
-            return fileName;
-        }
+//         public String uploadFile(MultipartFile file) throws IOException {
+//             String fileName = generateFileName(file);
+//             ObjectMetadata metadata = new ObjectMetadata();
+//             metadata.setContentLength(file.getSize());
+//             amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
+//             return fileName;
+//         }
 
-        private String generateFileName(MultipartFile file) {
-            return UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
-        }
-}
+//         private String generateFileName(MultipartFile file) {
+//             return UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+//         }
+// }
