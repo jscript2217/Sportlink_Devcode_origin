87efa4d8d0d2c808a5e9a885fead127444fe7b9f
jscript2217 <dlwjdtlr6@naver.com>
2024-08-05 23:36:19 +0900

    s3 config 수정

--- src/main/java/com/firstteam/sportsLink/S3Config.java
+++ src/main/java/com/firstteam/sportsLink/S3Config.java
@@ -2,7 +2,7 @@ package com.firstteam.sportsLink;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +10,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${spring.cloud.aws.credentials.accessKey}")
+
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
-    @Value("${spring.cloud.aws.credentials.secretKey}")
+
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
-    @Value("${spring.cloud.aws.region.static}")
+
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
